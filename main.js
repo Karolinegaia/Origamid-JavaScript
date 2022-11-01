9f4e02077b898fd37b54053a803dcadf1d988992
Karolinegaia <drakarolinegaia@gmail.com>
2022-11-01 10:54:00 -0300

    Exercício de Variáveis

---
+++ main.js
@@ -0,0 +1,20 @@
+// Declarar uma variável com o seu nome
+var nome = 'Karoline';
+console.log(nome);
+
+// Declarar uma variável com a sua idade
+var idade = 30;
+console.log(idade);
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorida;
+console.log(comidaFavorida);
+
+// Atribuir valor a sua comida favorita
+var comida = 'macarronada';
+console.log(comida);
+
+// Declarar 5 variáveis diferentes sem valores
+var corFavorita, time, series, hobbies, livros;
+console.log(corFavorita, time, series, hobbies, livros);
