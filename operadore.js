68578973f67b177d8fdbac897f2605d0cc7e0a5d
Karolinegaia <drakarolinegaia@gmail.com>
2022-11-03 10:15:52 -0300

    Criando Operadores JS

---
+++ operadore.js
@@ -0,0 +1,8 @@
+var soma = 100 + 50; // 150
+var subtracao = 100 - 50; // 50
+var multiplicacao = 100 * 2; // 200
+var divisao = 100 / 2; // 50
+var expoente = 2 ** 4; // 16
+var modulo = 14 % 5; // 4
+
+console.log(soma, subtracao, multiplicacao,divisao,expoente,modulo);
