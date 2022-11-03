15bd9487a7a35036fb62b6a4a4b723589b153d25
Karolinegaia <drakarolinegaia@gmail.com>
2022-11-03 10:03:12 -0300

    Exercício de Sting

--- main.js
+++ main.js
@@ -18,3 +18,25 @@ console.log(comida);
 // Declarar 5 variáveis diferentes sem valores
 var corFavorita, time, series, hobbies, livros;
 console.log(corFavorita, time, series, hobbies, livros);
+
+// Declare uma variável contendo uma string
+var livroFavorito = 'Harry Potter';
+console.log(livroFvaorito);
+
+// Declare uma variável contendo um número dentro de uma string
+var nomeIdade = `Karoline tem ${30} anos`
+console.log(nomeIdade);
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = 'Karoline';
+var sobrenome = 'Gaia';
+console.log(nome + ' ' +sobrenome);
+// Coloque a seguinte frase em uma variável: It's time
+
+var frase = "It's time";
+console.log(frase);
+
+// Verifique o tipo da variável que contém o seu nome
+var nome = 'Karoline';
+console.log(typeof nome);
