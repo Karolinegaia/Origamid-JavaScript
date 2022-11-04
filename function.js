2b169c06d0c02b76df0b497fc4ce31791a458af0
Karolinegaia <drakarolinegaia@gmail.com>
2022-11-04 11:00:51 -0300

    Function e condicionais

--- function.js
+++ function.js
@@ -1,6 +1,20 @@
 //IMC
-function imc (peso, altura) {
-    return imc = peso / (altura * altura);    
-};
+// function imc (peso, altura) {
+//     return imc = peso / (altura * altura);    
+// };
 
-console.log(imc (78, 1.65));
+// console.log(imc (78, 1.65));
+
+
+function corFavorita(cor) {
+    if(cor === 'azul') {
+      return 'Você gosta do céu';
+    } else if(cor === 'verde') {
+      return 'Você gosta de árvores';
+    } else {
+      return 'Você não gosta de nada';
+    }
+  };
+
+  corFavorita('azul');
+  
