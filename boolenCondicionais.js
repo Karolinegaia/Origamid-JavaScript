fb18269aea20172cc076ff3b6ff42863a7cba5f8
Karolinegaia <drakarolinegaia@gmail.com>
2022-11-04 10:19:21 -0300

    Criando Boolean e Condicionais

---
+++ boolenCondicionais.js
@@ -0,0 +1,11 @@
+var possuiGraduacao = true;
+var possuiDoutorado = false;
+
+if(possuiDoutorado) {
+  console.log('Possui graduação e doutorado');
+} else if(possuiGraduacao) {
+  console.log('Possui graduação, mas não possui doutorado');
+} else {
+  console.log('Não possui graduação');
+}
+
