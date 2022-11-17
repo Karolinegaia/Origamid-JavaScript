699a00971975d7181350190ec48426f849f9b70b
Karolinegaia <drakarolinegaia@gmail.com>
2022-11-17 09:43:51 -0300

    Estudando Dom

---
+++ dom.js
@@ -0,0 +1,9 @@
+const titulo = document.querySelector('h1');
+
+titulo.innerText; // retorna o texto;
+titulo.classList; // retorna as classes;
+titulo.id; // retorna o id;
+titulo.offsetHeight; // retorna a altura do elemento;
+
+titulo.addEventListener('click', callback);
+// ativa a função callback ao click no titulo
